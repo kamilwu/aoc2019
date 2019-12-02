a40f2f339b8621f0f2803bb6a6c55fb193ae5ab8
kamilwu <kami36@poczta.fm>
2019-12-02 22:05:59 +0100

    Added day 1

---
+++ day1.py
@@ -0,0 +1,23 @@
+import math
+
+
+def formula(mass):
+    return math.floor(mass / 3) - 2
+
+
+def calc_fuel_requirements():
+    with open('day1.txt') as file:
+        return [formula(int(x)) for x in file.readlines()]
+
+
+def calc_extra_fuel(fuel):
+    extra = formula(fuel)
+    return extra + calc_extra_fuel(extra) if extra > 0 else 0
+
+
+if __name__ == '__main__':
+    fuel_per_module = calc_fuel_requirements()
+    print(sum(fuel_per_module))
+
+    extra_fuel = [calc_extra_fuel(x) for x in fuel_per_module]
+    print(sum(fuel_per_module) + sum(extra_fuel))
