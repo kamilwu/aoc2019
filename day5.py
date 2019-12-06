709eea144fa916fac697e16cdd124758c783061d
Kamil Wasilewski <kamil.wasilewski@polidea.com>
2019-12-06 11:15:04 +0100

    Added Day 5 part 2

--- day5.py
+++ day5.py
@@ -5,63 +5,57 @@ BUFFER = []
 
 
 def program_loop(program, input_value):
+    program = list(program)
     cir = 0  # current instruction register
 
     while cir < len(program):
         value = program[cir]
-        offset = 1
+        offset = 0
 
         def parameter_modes():
             modes = str(int(value / 100)).zfill(3)
             for mode in modes[::-1]:
-                # nonlocal offset
-                # offset += 1
+                nonlocal offset
+                offset += 1
                 yield mode
 
-        opcode = value % 100
-        parameter_modes_gen = parameter_modes()
+        def address_gen():
+            parameter_modes_gen = parameter_modes()
+            for i in [1, 2, 3]:
+                mode = next(parameter_modes_gen)
+                idx = cir + i
+                yield program[idx] if mode == '0' else idx
 
-        def address(param):
-            mode = next(parameter_modes_gen)
-            idx = cir + param
-            return program[idx] if mode == '0' else idx
+        opcode = value % 100
+        addr = address_gen()
 
         if opcode == 1:
-            inputs = program[address(1)], program[address(2)]
-            program[address(3)] = add(*inputs)
-            offset = 3
+            inputs = program[next(addr)], program[next(addr)]
+            program[next(addr)] = add(*inputs)
         elif opcode == 2:
-            inputs = program[address(1)], program[address(2)]
-            program[address(3)] = mul(*inputs)
-            offset = 3
+            inputs = program[next(addr)], program[next(addr)]
+            program[next(addr)] = mul(*inputs)
         elif opcode == 3:
-            inputs = program[address(1)]
-            program[inputs] = input_value
+            program[next(addr)] = input_value
         elif opcode == 4:
-            inputs = program[address(1)]
+            inputs = program[next(addr)]
             BUFFER.append(inputs)
         elif opcode == 5:
-            cond = program[address(1)]
-            ptr = program[address(2)]
+            cond = program[next(addr)]
+            ptr = program[next(addr)]
             if cond:
                 offset = ptr - cir - 1
-            else:
-                offset = 2
         elif opcode == 6:
-            cond = program[address(1)]
-            ptr = program[address(2)]
+            cond = program[next(addr)]
+            ptr = program[next(addr)]
             if not cond:
                 offset = ptr - cir - 1
-            else:
-                offset = 2
         elif opcode == 7:
-            program[address(3)] = 1 if program[address(1)] < program[
-                address(2)] else 0
-            offset = 3
+            cond = program[next(addr)] < program[next(addr)]
+            program[next(addr)] = 1 if cond else 0
         elif opcode == 8:
-            program[address(3)] = 1 if program[address(1)] == program[
-                address(2)] else 0
-            offset = 3
+            cond = program[next(addr)] == program[next(addr)]
+            program[next(addr)] = 1 if cond else 0
         elif opcode == 99:
             return program
 
@@ -91,14 +85,17 @@ if __name__ == '__main__':
         999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99
     ]
     program_loop(test_input, 7)
+    assert len(BUFFER) == 1
     result = BUFFER.pop()
     assert result == 999, 'actual %s' % result
 
     program_loop(test_input, 8)
+    assert len(BUFFER) == 1
     result = BUFFER.pop()
     assert result == 1000, 'actual %s' % result
 
     program_loop(test_input, 9)
+    assert len(BUFFER) == 1
     result = BUFFER.pop()
     assert result == 1001, 'actual %s' % result
 
